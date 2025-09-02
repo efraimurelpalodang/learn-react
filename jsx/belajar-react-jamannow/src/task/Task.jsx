d480c8a9fa7b00b71a0e946c6122456785e1145c
efraim urel <efraimurelpalodang@gmail.com>
2025-09-02 10:41:25 +0800

    eksplisit (memisahkan menjadi beberapa bagian di task)

--- jsx/belajar-react-jamannow/src/task/Task.jsx
+++ jsx/belajar-react-jamannow/src/task/Task.jsx
@@ -1,37 +1,20 @@
-import { useState } from "react";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export default function Task() {
-  const [item, setItem] = useState("");
   const [items, setItems] = useImmer([]);
 
-  function handleChange(e) {
-    setItem(e.target.value);
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-    setItems((draft) => {
+  function handleSubmit(item) {
+    setItems(draft => {
       draft.push(item);
     });
-    setItem("");
   }
 
   return (
     <div>
-      <h1>Create Task</h1>
-      <form>
-        <input value={item} onChange={handleChange}/>
-        <button onClick={handleClick}>Add</button>
-      </form>
-      <h1>List Task</h1>
-      <ul>
-        {
-          items.map((item, index) => {
-            return <li key={index}>{item}</li>
-          })
-        }
-      </ul>
+      <TaskForm onSubmit={handleSubmit} />
+      <TaskList items={items}/>
     </div>
   )
 }
