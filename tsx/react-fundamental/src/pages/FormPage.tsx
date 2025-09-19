bb12674d509f07bbf7db97bf568b34a47d304cb7
efraim urel <efraimurelpalodang@gmail.com>
2025-09-20 07:01:19 +0800

    learn handle error input in react, tapi masih yang fundamental yang ribetnya

--- tsx/react-fundamental/src/pages/FormPage.tsx
+++ tsx/react-fundamental/src/pages/FormPage.tsx
@@ -7,12 +7,20 @@ export default function FormPage() {
 
   // controlled component/input
   const [fullNameInput, setFullNameInput] = useState("");
-  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [usernameErrorMessage, setUsernameErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
 
   const handleSubmit = () => {
-    const fullNameFormVal = InputNameRef.current?.value;
-    const EmailFormVal = InputEmailRef.current?.value;
-    alert(`hasil ketikan : ${fullNameFormVal}, ${EmailFormVal}`);
+    // const fullNameFormVal = InputNameRef.current?.value;
+    // const EmailFormVal = InputEmailRef.current?.value;
+    // alert(`hasil ketikan : ${fullNameFormVal}, ${EmailFormVal}`);
+    if(fullNameInput.length < 3) {
+      setUsernameErrorMessage("username minimal 3 karakter");
+    } 
+    if(passwordInput.length < 8) {
+      setPasswordErrorMessage("password minimal 8 karakter");
+    }
   };
 
   return (
@@ -20,13 +28,22 @@ export default function FormPage() {
       <h1>Form Page</h1>
 
       <h3>{fullNameInput}</h3>
-      <h3>{emailInput}</h3>
+      <h3>{passwordInput}</h3>
 
       <div style={{ display: "flex", flexDirection: "column", gap: "7px", maxWidth: "200px" }}>
-        <label htmlFor="name">Your Full Name</label>
-        <input type="text" onChange={(e) => setFullNameInput(e.target.value)} name="name" id="name" autoComplete="off" value={fullNameInput} />
-        <label htmlFor="email">Your email</label>
-        <input type="email" onChange={(e) => setEmailInput(e.target.value)} name="email" id="email" autoComplete="off" value={emailInput}/>
+        <label htmlFor="username">Username</label>
+        <input type="text" onChange={(e) => {
+          setFullNameInput(e.target.value);
+          if(e.target.value.length < 3) {
+            setUsernameErrorMessage("username minimal 3 karakter");
+          } else {
+            setUsernameErrorMessage("");
+          }
+          }} name="username" id="username" autoComplete="off" value={fullNameInput} />
+        <span style={{ color: "red", fontSize: "12px", marginTop: "-5px" }}>{usernameErrorMessage}</span>
+        <label htmlFor="password">Password</label>
+        <input type="password" onChange={(e) => setPasswordInput(e.target.value)} name="pass" id="pass" autoComplete="off" value={passwordInput} />
+        <span style={{ color: "red", fontSize: "12px", marginTop: "-5px" }}>{passwordErrorMessage}</span>
         <button type="submit" onClick={handleSubmit}>
           submit
         </button>
