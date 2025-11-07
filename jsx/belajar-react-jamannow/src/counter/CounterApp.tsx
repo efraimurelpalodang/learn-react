77a4591e018292f24ef1fa754ada14b94f859bd0
efraim urel <efraimurelpalodang@gmail.com>
2025-11-07 17:41:57 +0800

    mereset state juga bisa digunakan dengan cara mengubah dom nya kaya di contoh counter/counter.tsx

--- jsx/belajar-react-jamannow/src/counter/CounterApp.tsx
+++ jsx/belajar-react-jamannow/src/counter/CounterApp.tsx
@@ -3,19 +3,27 @@ import Counter from "./Counter";
 import type { Change } from "../ts/types";
 
 const CounterApp = () => {
-  const [show2, setShow2 ] = useState(true);
+  const [show2, setShow2] = useState(true);
 
   const handleChange = (e: Change) => {
     setShow2(e.target.checked);
-  }
+  };
 
   return (
-   <>
+    <>
     {/* <Counter nama="Jhon Dae"/> */}
-    {show2 ? <Counter nama="Jhon Dae" /> : <p>Hilang</p>}
+    {show2 ? (
+      <div>
+      <Counter nama="Jhon Dae" />
+      </div>
+    ) : (
+      <section>
+      <Counter nama="Alice" />
+      </section>
+    )}
     <input type="checkbox" checked={show2} onChange={handleChange} />
-   </>
+    </>
   );
-}
+};
 
 export default CounterApp;
